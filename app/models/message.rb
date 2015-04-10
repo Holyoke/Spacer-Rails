8eac56149613c0cb0f071d1d69b7b0c11a95409c
Holyoke <kid.idioteque@gmail.com>
2015-04-10 00:30:31 -0700

    Reimagine correct_text

--- app/models/message.rb
+++ app/models/message.rb
@@ -16,7 +16,20 @@ class Message < ActiveRecord::Base
 	before_validation :correct_text, on: :new
 
 	def correct_text
-		self.converted_text = body.gsub(".", ". ")
-		self.corrections = body.gsub(".", "<mark>.</mark>")
+		self.converted_text = self.body
+		self.corrections = self.body
+
+		body.each_char.with_index do |c, i|
+			if c == "."
+				check_period(i)
+			end
+		end
+
+		nil
+	end
+
+	def check_period(i)
+		self.converted_text = body.sub(".", ". ")
+		self.corrections = body.sub(".", "<mark>.</mark ")
 	end
 end
